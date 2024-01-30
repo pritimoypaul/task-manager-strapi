a3ee7be8694449ff4e0f3a67fdc29435e3128188
Pritimoy Paul <pritimoy97@gmail.com>
2024-01-30 17:25:42 +0600

    complete api

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import {
@@ -11,23 +12,66 @@ import {
 
 import uuid from "react-native-uuid";
 
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    Authorization:
+      "Bearer f48c0103dd25e3d9a8cf44ea30cbe7ad42b135cc2d23b04746a877b76da6f4c3764ce9701ebebd2aabf1048ad1e0b16bc5a55d670c5e5c3e16265523b41379492c52b3b7bf1f6d1e090d7743bf46fd43e7fbde97dcd632d9fedb3600c6b4c917289567ad96179837be8f5c24c97aecf8477d592578ee7162ebb27d087d0e7dc4",
+  },
+};
+
 export default function App() {
   const [input, setInput] = useState("");
-  const [tasks, setTasks] = useState([
-    {
-      id: uuid.v4(),
-      isDone: false,
-      name: "sidfji odis ji",
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
-  const createTask = async () => {};
+  const createTask = async () => {
+    axios
+      .post(
+        "http://10.0.2.2:1337/api/tasks",
+        {
+          data: {
+            name: input,
+          },
+        },
+        config
+      )
+      .then((res) => {
+        getData();
+      });
+  };
 
-  const setNewState = (id) => {};
+  const setNewState = (id, isDone) => {
+    axios
+      .put(
+        `http://10.0.2.2:1337/api/tasks/${id}`,
+        {
+          data: {
+            isDone: isDone,
+          },
+        },
+        config
+      )
+      .then((res) => {
+        getData();
+      });
+  };
 
-  const deleteTask = (id) => {};
+  const deleteTask = (id) => {
+    axios.delete(`http://10.0.2.2:1337/api/tasks/${id}`, config).then((res) => {
+      getData();
+    });
+  };
 
-  const getData = async () => {};
+  const getData = async () => {
+    axios
+      .get("http://10.0.2.2:1337/api/tasks", config)
+      .then((res) => {
+        setTasks(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
     getData();
@@ -82,13 +126,13 @@ export default function App() {
             <TouchableOpacity
               key={task?.id}
               onPress={() => {
-                setNewState(task?.id);
+                setNewState(task?.id, !task?.attributes?.isDone);
               }}
             >
               <View className="bg-white w-full flex-row justify-start items-center rounded-md h-[58]">
                 <Image
                   source={
-                    task?.isDone
+                    task?.attributes?.isDone
                       ? require("./assets/images/icons/task_done.png")
                       : require("./assets/images/icons/task.png")
                   }
@@ -97,16 +141,16 @@ export default function App() {
                   className="w-3/4"
                   style={{
                     textDecorationLine: `${
-                      task?.isDone ? "line-through" : "none"
+                      task?.attributes?.isDone ? "line-through" : "none"
                     }`,
                   }}
                 >
-                  {task?.name}
+                  {task?.attributes?.name}
                 </Text>
                 <View className="flex-1 flex-row justify-end items-center">
                   <TouchableOpacity
                     onPress={() => {
-                      deleteTask(task.id);
+                      deleteTask(task?.id);
                     }}
                   >
                     <Text className="text-red-500 font-bold">X</Text>
